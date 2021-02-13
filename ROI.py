d9dad5d0850549ebde6002920ff2c7634813f760
SilentWolf27 <97pedro@live.com.mx>
2021-02-13 14:00:44 -0600

    Regiones de interes

--- ROI.py
+++ ROI.py
@@ -4,7 +4,9 @@ import sys
 img = cv.imread('./images/grogu.jpg',cv.IMREAD_COLOR)
 
 roi = img[250:500, 250:500]
+roi = cv.bitwise_not(roi)
+img[250:500, 250:500] = roi
 
-cv.imshow('roi',roi)
+cv.imshow('roi',img)
 cv.waitKey(0)
 cv.destroyAllWindows()
