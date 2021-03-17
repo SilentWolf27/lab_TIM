0c4b38e4f6e9277529c428738b14af40de55262c
Pedro_Juarez <54186390+SilentWolf27@users.noreply.github.com>
2021-03-16 20:51:43 -0600

    umbralizado (#3)
    * filtros arbitrarios
    
    * Umbralizado

---
+++ Threshhold.py
@@ -0,0 +1,18 @@
+import cv2 as cv 
+
+imagen = cv.imread('./images/python-logo.png', cv.IMREAD_GRAYSCALE)
+ret, binario = cv.threshold(imagen, 120, 255, cv.THRESH_BINARY)
+ret, binario_inv = cv.threshold(imagen, 120, 255, cv.THRESH_BINARY_INV)
+ret, trunc = cv.threshold(imagen, 210, 255, cv.THRESH_TRUNC)
+ret, zero = cv.threshold(imagen, 120, 255, cv.THRESH_TOZERO)
+ret, zero_inv = cv.threshold(imagen, 120, 255, cv.THRESH_TOZERO_INV)
+
+cv.imshow('original', imagen)
+cv.imshow('binario', binario)
+cv.imshow('binario inv', binario_inv)
+cv.imshow('trunc', trunc)
+cv.imshow('zero', zero)
+cv.imshow('zero inv', zero_inv)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
