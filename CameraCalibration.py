a86185a629dcc68faeaf3e878d944e7034f981de
Pedro_Juarez <54186390+SilentWolf27@users.noreply.github.com>
2021-05-07 12:54:35 -0500

    Add files via upload

--- CameraCalibration.py
+++ CameraCalibration.py
@@ -15,7 +15,6 @@ imgpoints = []
 
 # Defining the world coordinates for 3D points
 objp = np.zeros((1, CHECKERBOARD[0] * CHECKERBOARD[1], 3), np.float32)
-print(objp)
 objp[0,:,:2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
 prev_img_shape = None
 
@@ -29,7 +28,7 @@ for fname in images:
     
     # Find the chess board corners
     # If desired number of corners are found in the image then ret = true
-    ret, corners = cv.findChessboardCorners(gray, CHECKERBOARD, cv.CALIB_CB_ADAPTIVE_THRESH + cv.CALIB_CB_FAST_CHECK + cv.CALIB_CB_NORMALIZE_IMAGE)
+    ret, corners = cv.findChessboardCorners(gray, CHECKERBOARD)
     
     """
     If desired number of corner are detected,
@@ -50,15 +49,10 @@ for fname in images:
     cv.waitKey(0)
 
 cv.destroyAllWindows()
-
+print(objpoints)
 h,w = img.shape[:2]
 
-"""
-Performing camera calibration by 
-passing the value of known 3D points (objpoints)
-and corresponding pixel coordinates of the 
-detected corners (imgpoints)
-"""
+
 ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
 
